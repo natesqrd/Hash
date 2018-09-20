d50b464c1f560e9c551bd0f30758e52bf7e5fcde
Nathaniel Tucker <ntucker463@student.msjc.edu>
2018-09-19 18:53:07 -0700

    Added Hash.h

--- Hash/Hash.h
+++ Hash/Hash.h
@@ -4,5 +4,26 @@
 template<class T>
 class Hash : public DblyLinkedList<T>
 {
+private:
+	int hashArray[hashSize];
+	static int hashSize = 50;
+	void initHash(int ts)
+	{
+		hashSize = ts;
+		for (int i = 0; i < hashSize; i++)
+		{
+			hashArray[i] = new Node<T>;
+		}
+	}
+	int hash(T);
+public:
+	Hash()
+	{
+		initHash(hashSize);
+	}
+	Hash(int ts)
+	{
+		initHash(ts);
+	}
 
 };
